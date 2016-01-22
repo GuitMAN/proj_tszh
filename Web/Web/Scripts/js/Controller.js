88c9b49e426d9b6a372d61d4b696318c9090775a
v_sayman <sayman0001111@gmail.com>
2016-01-22 11:22:33 +0300

    fix commit

--- Web/Web/Scripts/js/Controller.js
+++ Web/Web/Scripts/js/Controller.js
@@ -6,80 +6,78 @@ var phonecatApp = angular.module('HomeApp', ['ngRoute', 'ngResource']);
 /* Config */
 phonecatApp.config([
   '$routeProvider', '$locationProvider',
-  function($routeProvide, $locationProvider){
-    $routeProvide
-        .when('/',{
-          templateUrl:'home/article',
-          controller: 'PhoneListCtrl'
-        })
-        .when('/about/:artId',{
-          templateUrl:'home/article',
-          controller: 'PhoneListCtrl'
-        })
-        .when('/contact/:articles',{
-            templateUrl: 'home/article',
-          controller:'HomeCtrl'
-        })
-		.otherwise({
-          redirectTo: '/'
-        });
+  function ($routeProvide, $locationProvider) {
+      $routeProvide
+          .when('/', {
+              templateUrl: 'home/article',
+              controller: 'PhoneListCtrl'
+          })
+          .when('/about/:artId', {
+              templateUrl: 'home/article',
+              controller: 'PhoneListCtrl'
+          })
+          .when('/contact/:articles', {
+              templateUrl: 'home/article',
+              controller: 'HomeCtrl'
+          })
+          .otherwise({
+              redirectTo: '/'
+          });
   }
 ]);
 
 /* Factory */
 
 phonecatApp.factory('Article', [
-  '$resource', function($resource) {
+  '$resource', function ($resource) {
       return $resource('home/getarticle/:articles',
           {
               method: 'getTask',
               artId: 'articles'
           }, // Query parameters
-          {'query': { method: 'GET' }
-    
-    });
-    //Phone.update(params, successcb, errorcb);
+          {
+              'query': { method: 'GET' }
+
+          });
+      //Phone.update(params, successcb, errorcb);
   }
 ]);
 
 /* Filter */
-phonecatApp.filter('checkmark', function() {
-  return function(input) {
-    return input ? '\u2713' : '\u2718';
-  }
+phonecatApp.filter('checkmark', function () {
+    return function (input) {
+        return input ? '\u2713' : '\u2718';
+    }
 });
 
-phonecatApp.controller('PhoneListCtrl',[
-  '$scope','$http', '$location', 'Article',
-  function ($scope, $http, $location, Article)
-  {
-     $scope.article = Article.query();
-          
-     console.log("Вывод - ", $scope.article.title);
+phonecatApp.controller('PhoneListCtrl', [
+  '$scope', '$http', '$location', 'Article',
+  function ($scope, $http, $location, Article) {
+      $scope.article = Article.query();
 
-    //Phone.query(params, successcb, errorcb)
+      console.log("Вывод - ", $scope.article.title);
 
-    //Phone.get(params, successcb, errorcb)
+      //Phone.query(params, successcb, errorcb)
 
-    //Phone.save(params, payloadData, successcb, errorcb)
+      //Phone.get(params, successcb, errorcb)
 
-    //Phone.delete(params, successcb, errorcb)
+      //Phone.save(params, payloadData, successcb, errorcb)
+
+      //Phone.delete(params, successcb, errorcb)
 
   }
 ]);
 
 /* Phone Detail Controller */
-phonecatApp.controller('HomeCtrl',[
-  '$scope','$http', '$location', '$routeParams', 'Article',
-  function($scope, $http, $location, $routeParams, Article) {
-    $scope.artId = $routeParams.artId;
+phonecatApp.controller('HomeCtrl', [
+  '$scope', '$http', '$location', '$routeParams', 'Article',
+  function ($scope, $http, $location, $routeParams, Article) {
+      $scope.artId = $routeParams.artId;
 
-    Article.get({artId: $routeParams.artId}, function(data) {
-      $scope.article = data;
-      //data.$save();
-    });
+      Article.get({ artId: $routeParams.artId }, function (data) {
+          $scope.article = data;
+          //data.$save();
+      });
 
   }
 ]);
-
-
