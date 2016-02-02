313e299c5d937b64f97765815967442f1d5f6f3b
v_sayman <sayman0001111@gmail.com>
2016-02-02 20:29:34 +0300

    fix

--- Web/Web/Controllers/AdmtszhController.cs
+++ Web/Web/Controllers/AdmtszhController.cs
@@ -454,7 +454,18 @@ namespace Web.Controllers
                 return new HttpStatusCodeResult(200);
         }
 
+        [HttpPost]
+        public JsonResult TheAction(string data)
+        {
 
+            string _jsonObject = data.Replace(@"\", string.Empty);
+            var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            Dictionary<string, string> jsonObject = serializer.Deserialize<Dictionary<string, string>>(_jsonObject);
+
+
+            return Json("ok");
+
+        }
 
 
 
